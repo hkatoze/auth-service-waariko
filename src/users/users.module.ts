9631e1433eacaea63a062235aa772938fc5fb8f2
Haroun <harounakinda.pro@gmail.com>
2026-02-13 17:13:47 +0000

    kkkkk

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthModule } from 'src/auth/auth.module';
 import { UsersController } from './users.controller';
-import { PrismaModule } from '../prisma/prisma.module';
-import { AuthModule } from '../auth/auth.module';
+ 
 
 
 
 
 @Module({
-  imports: [PrismaModule, forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
 })
