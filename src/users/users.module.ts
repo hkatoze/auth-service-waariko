34e2cf070d502e2087b7c87d37d0e59f209bdafd
Haroun <harounakinda.pro@gmail.com>
2026-02-13 14:16:04 +0000

    Auth module added to UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+
+
+
 
 @Module({
-  providers: [UsersService],
+ imports: [
+    PrismaModule,
+    AuthModule, 
+  ],
   controllers: [UsersController],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
